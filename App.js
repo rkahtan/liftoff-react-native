2e8376f9bca4d8e018a8462022214867b07960c1
Rachel Kahtan <kahtan.rachel@gmail.com>
2021-08-02 20:35:01 -0400

    start auth form component

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NativeRouter } from 'react-router-native';
+import { Login, Signup } from './client/components/AuthForm';
+//how to choose which to render...
 
 //import store and do the provider stuff
 //import navbar, routes
